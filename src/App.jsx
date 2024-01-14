bd6981aba0fcbcb09b3ed05b4650d9d956787c96
Chen <xiaokunchen@outlook.com>
2024-01-14 00:54:36 -0500

    feat: add router and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { useEffect, useState } from 'react';
 import './App.css'
 import Card from './components/Card.jsx'
 import GameDetail from './components/GameDetail.jsx';
-function App() {
+import { Outlet } from 'react-router-dom';
+import { getGames } from '../lib/api'
+
+function TempHome() {
   const [gameList, setGameList] = useState([]);
   const [isOverlayVisible, setIsOverlayVisible] = useState(false);
   const [selectedGameId, setSelectedGameId] = useState(null);
@@ -45,7 +48,7 @@ function App() {
   }
 
   return (
-    <div className='home'>
+    <div className='app'>
       {gameList.map((response) => {
         const game = response; 
          
@@ -75,35 +78,17 @@ function App() {
   )
 }
 
-// Function marked as async to use await inside
-async function getGames() {
-  try {
-    let url = new URL("https://api.rawg.io/api/games");
 
-    // Setup the parameters
-    let params = {
-      key: '47e36d6273bc45acad4e17f724d8eaf7', // Replace with your actual API key
-      page_size: '15'      // Number of results to return per page
-    };
-
-    // Append parameters to the URL
-    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
-
-    // Await the fetch call to resolve, getting the Response object
-    let response = await fetch(url);
-
-    // Await the parsing of the Response body as JSON
-    let data = await response.json();
-
-    // The data variable now holds the parsed JSON object
-    console.log(data);
-
-    // If you need to use the data outside, you can return it
-    return data;
-  } catch (error) {
-    console.error('Error fetching games:', error);
-  }
+function App() {
+  return (
+    <div className="app">
+      <Outlet />
+    </div>
+  );
 }
+
+// Function marked as async to use await inside
+
 
 // Call the function
 getGames();
