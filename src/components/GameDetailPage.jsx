6eeba5c995f1ed73c5e7fbe5cdaac435d0457f6c
Chen <xiaokunchen@outlook.com>
2024-01-18 22:34:46 -0500

    feat: add screenshot slider; add styles

--- src/components/GameDetailPage.jsx
+++ src/components/GameDetailPage.jsx
@@ -1,23 +1,40 @@
 import { useParams } from "react-router-dom";
 import "../styles/GameDetailPage.css"
-import PropTypes from 'prop-types'
 import Slider from "react-slick";
 import { useEffect, useState } from "react";
-import { getGameDetails } from "../../lib/api";
+import { getGameDetails, getGameScreenshots } from "../../lib/api";
 
 
 function GameDetailPage(props) {
     const {id} = useParams();
     const [gameDetail, setGameDetail] = useState({});
-    console.log(id);
+    const [gameScreenshots, setGameScreenshots] = useState([]);
+
     useEffect(() => {
         async function fetchGameDetail() {
             const gamedetail = await getGameDetails(id);
             setGameDetail(gamedetail);
         }
 
+        async function fetchGameScreenshots() {
+            const screenshots = await getGameScreenshots(id);
+            setGameScreenshots(screenshots);
+        }
+
         fetchGameDetail();
+        fetchGameScreenshots();
+
     }, [id]);
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+      };
 
     return (
         <div className="GameDetailPage">
@@ -27,8 +44,13 @@ function GameDetailPage(props) {
                 </div>
                 <div className="game-album">
                     <div className="album-slider-container">
-                        <Slider>
-
+                        <Slider {...settings}>
+                            {<img className="detail-album-img" src={gameDetail.background_image} alt={gameDetail.name} />}
+                            {gameScreenshots.map((screenshot) => {
+                                return (
+                                        <img className="album-img" src={screenshot.image} alt={gameDetail.name} key={gameDetail.id}/>
+                                );
+                            })}
                         </Slider>
                     </div>
                 </div>
